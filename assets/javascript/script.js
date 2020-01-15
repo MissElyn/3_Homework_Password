1e1aa7905a525866881781c73a4893af5a9d23d2
Ellen Cianflone <ellencianflone@gmail.com>
2020-01-14 23:25:35 -0500

    try

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+// need to write a script that is linked to the index file. The class id is rGenerator located in the index file.//
+
+// The js file will need to prompt the user to choose a password between 8-28 char.//
+
+
+
+var prompt("Please enter your password here.");
