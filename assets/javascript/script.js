71956594cf4eee282e6e1e6b534dd324acfd444f
Ellen Cianflone <ellencianflone@gmail.com>
2020-01-16 22:06:39 -0500

    adding javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,5 +3,19 @@
 // The js file will need to prompt the user to choose a password between 8-28 char.//
 
 
+var gPassword=document.getElementById("gPassword");
 
-var prompt("Please enter your password here.");
+
+gPassword.addEventListener("click", function(){
+        //generate password//
+
+        function generatePassword() {
+            var length = 10,
+                charset = "abcdefghijklmnopqrstuvwxyz",
+                retVal = "";
+            for (var i = 0, n = charset.length; i < length; ++i) {
+                retVal += charset.charAt(Math.floor(Math.random() * n));
+            }
+            return retVal;
+        
+        }
