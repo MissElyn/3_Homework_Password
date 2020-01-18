ff6a463dde1eb18ea5ae0352b9fab8ca6609605a
Ellen Cianflone <ellencianflone@gmail.com>
2020-01-18 12:27:19 -0500

    Basic 10 char

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 3_Homework_Password
 Random Password Generator. Due January 18, 2020
+
+As per the request, we the developer will be required to create a javascript program that when the user clicks on the generate button, a random password will display.
+
+The password must between 8-128 characters in legnth and contain at least 
+        one(1) number; 
+        one(1) upper-case alphnumaric character;
+        one(1) lower-case alphnumaric character;
+        one(1) special character.
+
+The design of the page is per user request and should ultimately have two(2) buttons. One to generate and display a password and the second will copy that password to the user's clipboard.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,37 @@
 // need to write a script that is linked to the index file. The class id is rGenerator located in the index file.//
 
-// The js file will need to prompt the user to choose a password between 8-28 char.//
+// The js file will need to randomly generate a password between 8-128 char.//
 
 
-var gPassword=document.getElementById("gPassword");
+var charU = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!$%^&*()-=+[]{};#:@~,./<>?";
+var charL  = "";
+var num = "";
+var sym = "";
 
 
-gPassword.addEventListener("click", function(){
-        //generate password//
 
-        function generatePassword() {
-            var length = 10,
-                charset = "abcdefghijklmnopqrstuvwxyz",
-                retVal = "";
-            for (var i = 0, n = charset.length; i < length; ++i) {
-                retVal += charset.charAt(Math.floor(Math.random() * n));
-            }
-            return retVal;
-        
+
+var passwd=document.getElementById("passwd");
+var gPasswd=document.getElementById("gPasswd");
+
+gPasswd.addEventListener("click", function()
+
+{       
+
+  var length = 10;  
+  var password = "";
+
+            //for loop
+
+        for (i=0; i < length; i++) {
+          var c = Math.floor(Math.random()
+          *charU.length);
+          password += charU.charAt(c)
         }
+        
+
+
+passwd.value = password;
+      
+      
+});
